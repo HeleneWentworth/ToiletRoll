1d98b5d9148371b2a21ede3f97434e0ff7ed553c
Helene <180022@virtualwindow.co.za>
2020-05-18 11:19:06 +0200

    Initial Commit

---
+++ ToiletRoll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToiletRoll
+//
+//  Created by Helene Wentworth on 2020/05/18.
+//  Copyright © 2020 Helene Wentworth. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
